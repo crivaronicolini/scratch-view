fa1523d47e487fbe08dfff92d5cf163a8075927c
marco <crivaronicolini@hotmail.com>
2023-04-19 15:15:10 -0300

    add licence to scratch-view

--- scratch-view.py
+++ scratch-view.py
@@ -1,4 +1,5 @@
 """ Scratch View: Analizador de ensayos de rayado.
+Licence at the end of the file.
 """
 
 __author__ = "Marco Crivaro Nicolini <mcn.hola@gmail.com>"
@@ -676,3 +677,20 @@ if __name__ == '__main__':
     # Show the viewer and run the application.formlayout
     # mainwindow.show()
     sys.exit(app.exec())
+
+"""
+Copyright (C) 2023  Marco Crivaro Nicolini
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
